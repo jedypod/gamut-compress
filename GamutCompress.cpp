000bdc32823574225bbe4960487696310a17389d
Jed Smith <jedypod@gmail.com>
2020-05-23 23:09:56 -0400

    Add log method, optimize and bugfix bisection solver

--- GamutCompress.cpp
+++ GamutCompress.cpp
@@ -7,7 +7,7 @@ kernel GamutCompression : ImageComputationKernel<ePixelWise> {
     float cyan;
     float magenta;
     float yellow;
-    int method; // compression method
+    int method;
     bool invert;
 
   local:
@@ -24,15 +24,15 @@ kernel GamutCompression : ImageComputationKernel<ePixelWise> {
     // lim is the max distance from the gamut boundary that will be compressed
     // 0 is a no-op, 1 will compress colors from a distance of 2.0 from achromatic to the gamut boundary
     // if method is Reinhard, use the limit as-is
-    if (method == 0) {
+    if (method == 1) {
       lim = float3(cyan+1, magenta+1, yellow+1);
     } else {
       // otherwise, we have to bruteforce the value of limit 
-      // such that lim is the value of x where y=1 - also enforce sane ranges to avoid nans
+      // such that lim is the value of x where y=1
       // importantly, this runs once at the beginning of evaluation, NOT per-pixel!!!
       lim = float3(
-        bisect(max(0.0001, cyan)+1), 
-        bisect(max(0.0001, magenta)+1), 
+        bisect(max(0.0001, cyan)+1),
+        bisect(max(0.0001, magenta)+1),
         bisect(max(0.0001, yellow)+1));
     }
   }
@@ -51,17 +51,24 @@ kernel GamutCompression : ImageComputationKernel<ePixelWise> {
   // compression function which gives the y=1 x intersect at y=0
   float f(float x, float k) {
     if (method == 0) {
-      return k;
+      // natural logarithm compression method
+      return (exp((1-thr+thr*log(1-x)-x*thr*log(1-x))/(thr*(1-x))))*thr+x*thr-k;
     } else if (method == 1) {
+      return k;
+    } else if (method == 2) { 
       // natural exponent compression method
       return -log((-x+1)/(thr-x))*(-thr+x)+thr-k;
-    } else if (method == 2) {
+    } else if (method == 3) {
       // arctangent compression method
       return (2*tan( (pi*(1-thr))/(2*(x-thr)))*(x-thr))/pi+thr-k;
-    } else if (method == 3) {
+    } else if (method == 4) {
       // hyperbolic tangent compression method
       return atanh((1-thr)/(x-thr))*(x-thr)+thr-k;
     }
+  }
+
+  int sign(float x) {
+    return x == 0 ? 0 : x > 0 ? 1 : 0;
   }
 
   float bisect(float k) {
@@ -69,32 +76,48 @@ kernel GamutCompression : ImageComputationKernel<ePixelWise> {
     // returns an approximation of the value of limit 
     // such that the compression function intersects y=1 at desired value k
     // this allows us to specify the max distance we will compress to the gamut boundary
+    
+    float a, b, c, y;
+    float tol = 0.0001; // accuracy of estimate
+    int nmax = 100; // max iterations
 
-    // initial guess
-    float a = 1.001;
-    float b = 100.0;
-
-    // verify guess: this should always be true with the functions we have enabled
-    bool good_guess = (f(a, k) < 0 && f(b, k) > 0) || (f(a, k) > 0 && f(b, k) < 0);
-    if (good_guess != 1) {
-      return 1;
+    // set up reasonable initial guesses for each method given output ranges of each function
+    if (method == 0) {
+      // natural logarithm needs a limit between -inf (linear), and 1 (clip)
+      a = -5;
+      b = 0.96;
+    } else if (method == 4) {
+      // tanh needs more precision
+      a = 1.000001;
+      b = 5;
+    } else {
+      a = 1.0001;
+      b = 5;
     }
 
-    float c, v;
-    float tolerance = 0.0001;
-    int nmax = 500; // max iterations
-    int n = 0; // current iteration
-    while (n <= nmax) {
-      c = (a + b)/2; // midpoint between a and b
-      v = f(c, k); // function value
-      if (v == 0 || (b-a)/2 < tolerance) {
-        break; // result found: c will be returned after while loop
-      }
-      if (v > 0 && f(a, k) > 0) {
-        a = c; // c is new lower bound
+    if (sign(f(a, k)) == sign(f(b, k))) {
+      // bad estimate. return something close to linear
+      if (method == 2) {
+        return -100;
       } else {
-        b = c; // c is new upper bound
+        return 1.999999;
       }
+    }
+    c = (a+b)/2;
+    y = f(c, k);
+    if (y == 0) {
+      return c; // lucky guess
+    }
+
+    int n = 1;
+    while ((fabs(y) > tol) && (n <= nmax)) {
+      if (sign(y) == sign(f(a, k))) {
+        a = c;
+      } else {
+        b = c;
+      }
+      c = (a+b)/2;
+      y = f(c, k);
       n += 1;
     }
     return c;
@@ -108,31 +131,36 @@ kernel GamutCompression : ImageComputationKernel<ePixelWise> {
       cdist = dist;
     } else {
       if (method == 0) {
-        // simple Reinhard type compression suggested by Nick Shaw and Lars Borg
-        // https://community.acescentral.com/t/simplistic-gamut-mapping-approaches-in-nuke/2679/3
-        // https://community.acescentral.com/t/rgb-saturation-gamut-mapping-approach-and-a-comp-vfx-perspective/2715/52
-        // example plot: https://www.desmos.com/calculator/h2n8smtgkl
+        // natural logarithm compression method: https://www.desmos.com/calculator/hmzirlw7tj
+        // inspired by ITU-R BT.2446 http://www.itu.int/pub/R-REP-BT.2446-2019
+        if (invert == 0) {
+          cdist = thr*log(dist/thr-lim)-lim*thr*log(dist/thr-lim)+thr-thr*log(1-lim)+lim*thr*log(1-lim);
+        } else {
+          cdist = exp((dist-thr+thr*log(1-lim)-lim*thr*log(1-lim))/(thr*(1-lim)))*thr+lim*thr;
+        }
+      } else if (method == 1) {
+        // simple Reinhard type compression method: https://www.desmos.com/calculator/lkhdtjbodx
         if (invert == 0) {
           cdist = thr + 1/(1/(dist - thr) + 1/(1 - thr) - 1/(lim - thr));
         } else {
           cdist = thr + 1/(1/(dist - thr) - 1/(1 - thr) + 1/(lim - thr));
         }
-      } else if (method == 1) {
-        // natural exponent compression method: plot https://www.desmos.com/calculator/jf99glamuc
+      } else if (method == 2) {
+        // natural exponent compression method: https://www.desmos.com/calculator/s2adnicmmr
         if (invert == 0) {
           cdist = lim-(lim-thr)*exp(-(((dist-thr)*((1*lim)/(lim-thr))/lim)));
         } else {
           cdist = -log((dist-lim)/(thr-lim))*(-thr+lim)/1+thr;
         }
-      } else if (method == 2) {
-        // arctangent compression method: plot https://www.desmos.com/calculator/olmjgev3sl
+      } else if (method == 3) {
+        // arctangent compression method: https://www.desmos.com/calculator/h96qmnozpo
         if (invert == 0) {
           cdist = thr + (lim - thr) * 2 / pi * atan(pi/2 * (dist - thr)/(lim - thr));
         } else {
           cdist = thr + (lim - thr) * 2 / pi * tan(pi/2 * (dist - thr)/(lim - thr));
         }
-      } else if (method == 3) {
-        // hyperbolic tangent compression method: plot https://www.desmos.com/calculator/sapcakq6t1
+      } else if (method == 4) {
+        // hyperbolic tangent compression method: https://www.desmos.com/calculator/xiwliws24x
         if (invert == 0) {
           cdist = thr + (lim - thr) * tanh( ( (dist- thr)/( lim-thr)));
         } else {
@@ -152,8 +180,8 @@ kernel GamutCompression : ImageComputationKernel<ePixelWise> {
     float ach = max(rgb.x, max(rgb.y, rgb.z));
 
     // distance from the achromatic axis for each color component aka inverse rgb ratios
-    // distance is normalized by achromatic, so that 1.0 is at gamut boundary, and avoiding 0 div
-    float3 dist = ach == 0 ? float3(0, 0, 0) : fabs(rgb-ach)/ach; 
+    // distance is normalized by achromatic, so that 1.0 is at gamut boundary, avoid 0 div
+    float3 dist = ach == 0 ? float3(0, 0, 0) : (ach-rgb)/ach;
 
     // compress distance with user controlled parameterized shaper function
     float3 cdist = float3(
