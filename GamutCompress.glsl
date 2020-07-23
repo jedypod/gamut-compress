4486be71a19b3a6af762a8a4b97b5f5b48a8c28b
Jed Smith <jedypod@gmail.com>
2020-07-22 23:54:11 -0400

    add hexagonal method, separate threshold controls in all implementations.

--- GamutCompress.glsl
+++ GamutCompress.glsl
@@ -1,16 +1,17 @@
 uniform sampler2D frontTex, matteTex, selectiveTex;
-uniform float threshold, power, shd_rolloff, cyan, magenta, yellow, adsk_result_w, adsk_result_h;
-uniform bool linearize_acescct, invert;
-
+uniform float power, shd_rolloff, cyan, magenta, yellow, adsk_result_w, adsk_result_h;
+uniform int working_colorspace;
+uniform bool invert, hexagonal;
+uniform vec3 threshold;
 
 // calc hyperbolic tangent
 float tanh( float val) {
-    float f = exp(2.0*val);
-    return (f-1.0)/(f+1.0);
+  float f = exp(2.0*val);
+  return (f-1.0)/(f+1.0);
 }
 
 // Convert ACEScg to ACEScct
-float lin_to_ACEScct(float val) {
+float lin_to_acescct(float val) {
   if (val <= 0.0078125) {
     return 10.5402377416545 * val + 0.0729055341958355;
   } else {
@@ -19,11 +20,33 @@ float lin_to_ACEScct(float val) {
 }
 
 // Convert ACEScct to ACEScg
-float ACEScct_to_lin(float val) {
+float acescct_to_lin(float val) {
   if (val > 0.155251141552511) {
     return pow( 2.0, val*17.52 - 9.72);
   } else {
     return (val - 0.0729055341958355) / 10.5402377416545;
+  }
+}
+
+// Convert acescg to acescc
+float lin_to_acescc(float val) {
+  if (val <= 0.0) {
+    return -0.3584474886; // =(log2( pow(2.,-16.))+9.72)/17.52
+  } else if (val < pow(2.0, -15.0)) {
+    return (log2(pow(2.0, -16.0)+val*0.5)+9.72)/17.52;
+  } else { // (val >= pow(2.,-15))
+    return (log2(val)+9.72)/17.52;
+  }
+}
+
+// Convert acescc to acescg
+float acescc_to_lin(float val) {
+  if (val < -0.3013698630) { // (9.72-15)/17.52
+    return (pow(2.0, val*17.52-9.72) - pow(2.0, -16.0))*2.0;
+  } else if (val < (log2(65504.0)+9.72)/17.52) {
+    return pow(2.0, val*17.52-9.72);
+  } else { // (val >= (log2(HALF_MAX)+9.72)/17.52)
+    return 65504.0;
   }
 }
 
@@ -52,61 +75,85 @@ float compress(float x, float l, float t, float p, bool invert) {
 }
 
 void main() {
-    vec2 coords = gl_FragCoord.xy / vec2( adsk_result_w, adsk_result_h );
-    // source pixels
-    vec3 rgb = texture2D(frontTex, coords).rgb;
-    float alpha = texture2D(matteTex, coords).g;
-    float select = texture2D(selectiveTex, coords).g;
+  vec2 coords = gl_FragCoord.xy / vec2( adsk_result_w, adsk_result_h );
+  // source pixels
+  vec3 rgb = texture2D(frontTex, coords).rgb;
+  float alpha = texture2D(matteTex, coords).g;
+  float select = texture2D(selectiveTex, coords).g;
 
-    // linearize_acescct
-    if (linearize_acescct) {
-      rgb.x = ACEScct_to_lin(rgb.x);
-      rgb.y = ACEScct_to_lin(rgb.y);
-      rgb.z = ACEScct_to_lin(rgb.z);
-    }
+  // Working colorspace to linear
+  if (working_colorspace == 1) {
+    rgb.x = acescct_to_lin(rgb.x);
+    rgb.y = acescct_to_lin(rgb.y);
+    rgb.z = acescct_to_lin(rgb.z);
+  } else if (working_colorspace == 2) {
+    rgb.x = acescc_to_lin(rgb.x);
+    rgb.y = acescc_to_lin(rgb.y);
+    rgb.z = acescc_to_lin(rgb.z);
+  } 
 
-    // thr is the percentage of the core gamut to protect: the complement of threshold.
-    float thr = 1.0 - threshold;
+  // thr is the percentage of the core gamut to protect: the complement of threshold.
+  vec3 thr = vec3(max(0.00001, 1.0-threshold.x), max(0.00001, 1.0-threshold.y), max(0.00001, 1.0-threshold.z));
+  
+  // lim is the distance beyond the gamut boundary that will be compressed to the gamut boundary.
+  // lim = 0.2 will compress from a distance of 1.2 from achromatic to 1.0 (the gamut boundary).  
+  vec3 lim;
+  lim = vec3(cyan+1.0, magenta+1.0, yellow+1.0);
 
-    // lim is the distance beyond the gamut boundary that will be compressed to the gamut boundary.
-    // lim = 0.2 will compress from a distance of 1.2 from achromatic to 1.0 (the gamut boundary).  
-    vec3 lim;
-    lim = vec3(cyan+1.0, magenta+1.0, yellow+1.0);
+  // achromatic axis 
+  float ach = max(rgb.x, max(rgb.y, rgb.z));
 
-    // achromatic axis 
-    float ach = max(rgb.x, max(rgb.y, rgb.z));
+  // achromatic with shadow rolloff below shd_rolloff threshold
+  float ach_shd = 1.0-((1.0-ach)<(1.0-shd_rolloff)?(1.0-ach):(1.0-shd_rolloff)+shd_rolloff*tanh((((1.0-ach)-(1.0-shd_rolloff))/shd_rolloff)));
 
-    // achromatic with shadow rolloff below shd_rolloff threshold
-    float ach_shd = 1.0-((1.0-ach)<(1.0-shd_rolloff)?(1.0-ach):(1.0-shd_rolloff)+shd_rolloff*tanh((((1.0-ach)-(1.0-shd_rolloff))/shd_rolloff)));
+  // distance from the achromatic axis for each color component aka inverse rgb ratios
+  // distance is normalized by achromatic, so that 1.0 is at gamut boundary. avoid 0 div
+  vec3 dist;
+  dist.x = ach_shd == 0.0 ? 0.0 : (ach-rgb.x)/ach_shd;
+  dist.y = ach_shd == 0.0 ? 0.0 : (ach-rgb.y)/ach_shd;
+  dist.z = ach_shd == 0.0 ? 0.0 : (ach-rgb.z)/ach_shd;
 
-    // distance from the achromatic axis for each color component aka inverse rgb ratios
-    // distance is normalized by achromatic, so that 1.0 is at gamut boundary. avoid 0 div
-    vec3 dist;
-    dist.x = ach_shd == 0.0 ? 0.0 : (ach-rgb.x)/ach_shd;
-    dist.y = ach_shd == 0.0 ? 0.0 : (ach-rgb.y)/ach_shd;
-    dist.z = ach_shd == 0.0 ? 0.0 : (ach-rgb.z)/ach_shd;
+  // compress distance with user controlled parameterized shaper function
+  float sat;
+  vec3 csat, cdist;
+  if (hexagonal) {
+    // Based on Nick Shaw's variation on the gamut mapping algorithm 
+    // https://community.acescentral.com/t/a-variation-on-jeds-rgb-gamut-mapper/3060
+    sat = max(dist.x, max(dist.y, dist.z));
+    csat = vec3(
+      compress(sat, lim.x, thr.x, power, invert),
+      compress(sat, lim.y, thr.y, power, invert),
+      compress(sat, lim.z, thr.z, power, invert));
+    cdist = sat == 0.0 ? dist : vec3(
+      dist.x * csat.x / sat,
+      dist.y * csat.y / sat,
+      dist.z * csat.z / sat);
+  } else {
+    cdist = vec3(
+      compress(dist.x, lim.x, thr.x, power, invert),
+      compress(dist.y, lim.y, thr.y, power, invert),
+      compress(dist.z, lim.z, thr.z, power, invert));
+  }
 
-    // compress distance with user controlled parameterized shaper function
-    vec3 cdist = vec3(
-      compress(dist.x, lim.x, thr, power, invert),
-      compress(dist.y, lim.y, thr, power, invert),
-      compress(dist.z, lim.z, thr, power, invert));
+  // recalculate rgb from compressed distance and achromatic
+  // effectively this scales each color component relative to achromatic axis by the compressed distance
+  vec3 crgb = vec3(
+    ach-cdist.x*ach_shd,
+    ach-cdist.y*ach_shd,
+    ach-cdist.z*ach_shd);
 
-    // recalculate rgb from compressed distance and achromatic
-    // effectively this scales each color component relative to achromatic axis by the compressed distance
-    vec3 crgb = vec3(
-      ach-cdist.x*ach_shd,
-      ach-cdist.y*ach_shd,
-      ach-cdist.z*ach_shd);
+  // Linear to working colorspace
+  if (working_colorspace == 1) {
+    crgb.x = lin_to_acescct(crgb.x);
+    crgb.y = lin_to_acescct(crgb.y);
+    crgb.z = lin_to_acescct(crgb.z);
+  } else if (working_colorspace == 2) {
+    crgb.x = lin_to_acescc(crgb.x);
+    crgb.y = lin_to_acescc(crgb.y);
+    crgb.z = lin_to_acescc(crgb.z);
+  }
 
-    // transform to log
-    if (linearize_acescct) {
-      crgb.x = lin_to_ACEScct(crgb.x);
-      crgb.y = lin_to_ACEScct(crgb.y);
-      crgb.z = lin_to_ACEScct(crgb.z);
-    }
+  crgb = mix(rgb, crgb, select);
 
-    crgb = mix(rgb, crgb, select);
-
-    gl_FragColor = vec4(crgb, alpha);
+  gl_FragColor = vec4(crgb, alpha);
 }
